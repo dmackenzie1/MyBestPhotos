d9f4162cf8894fd439ff2951374f391703eb4d39
David MacKenzie <dmackenzie1@gmail.com>
2026-04-27 13:37:03 -0500

    fix: remove generated JS artifacts and prevent re-emission

--- services/app/client/src/routes.js
+++
@@ -1,8 +0,0 @@
-export const APP_ROUTES = {
-    browse: "/browse",
-    timeline: "/timeline",
-};
-export const APP_ROUTE_CONFIG = [
-    { path: APP_ROUTES.browse, mode: "browse" },
-    { path: APP_ROUTES.timeline, mode: "timeline" },
-];
